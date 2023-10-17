5749b4f0e6bf9a21a3337f392c342099ec1a553d
IRadoevic <iradojevic1@gmail.com>
2023-10-18 00:13:23 +0200

    pocetak html, css

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
-
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="index.css">
+        <title>Document</title>
     </head>
     <body>
-        HELLO WORLD!
-        <?php header("Location:.\back\db_con.php") ?>
+            <div id="wrapper">
+                <div id = "header"></div>
+                <div id = "regularOffers"></div>
+                <div id = "search"></div>
+                <div id = "offers">
+                    <div class="offerTemplate">
+                        <img src="image.png" alt="jagoda" id="slika">
+                    </div>
+                    <div class="offerTemplate">
+                        <img src="image.png" alt="jagoda" id="slika">
+                    </div>
+                    <div class="offerTemplate">
+                        <img src="image.png" alt="jagoda" id="slika">
+                    </div>
+                    <div class="offerTemplate">
+                        <img src="image.png" alt="jagoda" id="slika">
+                    </div>
+                </div>
+            </div>
+
+    
     </body>
 </html>
