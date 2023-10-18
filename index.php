61ce786cbde18f4934cdd3df11a16b64f10e4e62
IRadoevic <iradojevic1@gmail.com>
2023-10-18 20:24:06 +0200

    sredjena animacija za kartice i odradjen search

--- index.php
+++ index.php
@@ -10,8 +10,36 @@
             <div id="wrapper">
                 <div id = "header"></div>
                 <div id = "regularOffers"></div>
-                <div id = "search"></div>
+                <div id = "search">
+                    <div id="searchHolder">
+                        <input type="button" value="FIlteri" id="filterButton">
+                        <input type="search" name="pretragaDogadjaj" id="mainSearch">
+                        <input type="button" value="Pretrazi" id="searchButton">
+                    </div>
+                </div>
                 <div id = "offers">
+                    <div class="container">
+                        <div class="image">
+                            <img src="image.png">
+                        </div>
+                        <div class="overlay">
+                            <p>naziv</p>
+                            <p>opis</p>
+                            <p>mesto</p>
+                            <p>vreme</p>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="image">
+                            <img src="image.png">
+                        </div>
+                        <div class="overlay">
+                            <p>naziv</p>
+                            <p>opis</p>
+                            <p>mesto</p>
+                            <p>vreme</p>
+                        </div>
+                    </div>
                     <div class="container">
                         <div class="image">
                             <img src="image.png">
