edaeaac8725f5b7b9d6dee28e308f128a9eaf364
IRadoevic <iradojevic1@gmail.com>
2023-10-20 00:01:23 +0200

    skrs

---
+++ load.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="front\css\load.css">
+        <title>Document</title>
+    </head>
+    <body>
+            <div id="wrapper">
+                <div id = "header"></div>
+                <div id = "regularOffers"></div>
+                <div id = "search">
+                    <div id="searchHolder">
+                        <input type="button" value="FIlteri" id="filterButton">
+                        <input type="search" name="pretragaDogadjaj" id="mainSearch">
+                        <input type="button" value="Pretrazi" id="searchButton">
+                    </div>
+                </div>
+                <div id = "offers">
+                    <div class="container">
+                        <div class="image">
+                            <img src="image.png">
+                        </div>
+                        <div class="overlay">
+                            <p>naziv</p>
+                            <p>opis</p>
+                            <p>mesto</p>
+                            <p>vreme</p>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="image">
+                            <img src="image.png">
+                        </div>
+                        <div class="overlay">
+                            <p>naziv</p>
+                            <p>opis</p>
+                            <p>mesto</p>
+                            <p>vreme</p>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="image">
+                            <img src="image.png">
+                        </div>
+                        <div class="overlay">
+                            <p>naziv</p>
+                            <p>opis</p>
+                            <p>mesto</p>
+                            <p>vreme</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    
+    </body>
+</html>
