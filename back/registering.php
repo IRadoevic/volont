21f08775e1f531b788f7ce6c3dc2aec8499cfd0a
Fanix9 <filipjeremicceda@gmail.com>
2023-10-24 22:28:34 +0200

    logreg6

--- back/registering.php
+++ back/registering.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Logging in</title>
+        <title>Registering</title>
     </head>
     <body>
-        <h1> Logging in... </h1>
+        <h1> Registering... </h1>
         <?php
             
             include_once("../back/log_reg.php");
@@ -17,18 +17,19 @@
             $surname = $_POST['surname'];
             $pic = $_POST['pic'];
             $email = $_POST['email'];
-            $description = $_POST['gender'];
+            $description = $_POST['description'];
+            $gender = $_POST['gender'];
             $birth_date = $_POST['birth_date'];
 
-            $value = Login($username, $password);
-            if($value == 'username') header("?error_code='username'");
-            else if($value == 'password') header("?error_code='password'")
+            $value = Register($username, $password, $name, $surname, $pic, $email, $description, $gender, $birth_date)
+            
+            if(is_string($value)) header("?error_code='$value'");
             else
             {
-                $_SESSION['id'] = $value;
-                header("")
+                $_SESSION['id'] = Login($username, $password);
+                header("");
             }
-        
+
         ?>
     </body>
 </html>
